4fe13d9b5cff39d9e145398ae7e4a9948528da4f
Villa2230 <juanvillapre2230@gmail.com>
2020-07-03 00:55:03 -0500

    validation branch office

--- src/main/java/pe/edu/upc/Karwas/model/entity/BranchOffice.java
+++ src/main/java/pe/edu/upc/Karwas/model/entity/BranchOffice.java
@@ -13,6 +13,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -27,19 +30,24 @@ public class BranchOffice {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
+	@NotBlank(message = "Ingrese el distrito del local")
 	@Column(name = "district", length = 15, nullable = false)
     private String district;
     
+	@NotBlank(message = "Ingrese la dirección del local")
 	@Column(name = "address", length = 30, nullable = false)
     private String address;
 	
+	@NotNull(message = "Por favor, ingrese la compañia a la que pertenece el local")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "company_id")
 	private Company company;
 
+	@NotEmpty(message = "Ingrese los anuncios que pertenecen al local")
 	@OneToMany(mappedBy="branchOffice", fetch = FetchType.LAZY)
     private List<Announcement> announcements;
 
+	@NotEmpty(message = "Ingrese los usuarios que pertenecen al local")
 	@OneToMany(mappedBy = "branchOffice", fetch = FetchType.LAZY)
 	private List<UserBranchoffice> userBranchOffices;
 	
