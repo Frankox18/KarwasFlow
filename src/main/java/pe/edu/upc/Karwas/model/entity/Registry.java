ea5bfa917a5e77c84878453bc265807dbea113e1
Villa2230 <juanvillapre2230@gmail.com>
2020-07-03 02:46:43 -0500

    validation registry

--- src/main/java/pe/edu/upc/Karwas/model/entity/Registry.java
+++ src/main/java/pe/edu/upc/Karwas/model/entity/Registry.java
@@ -9,6 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -23,20 +26,27 @@ public class Registry {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotNull(message = "Ingrese el lavado")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "washed_id")
 	private Washed washed;
 	
+	@NotNull(message = "Ingrese el auto")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "car_id")
 	private Car car;
 	
+	@Min(0)
+	@Max(24)
 	@Column(name = "start_time_wash", length = 2, nullable = false)
 	private int startTimeWash;
 	
+	@Min(0)
+	@Max(24)
 	@Column(name = "end_time_wash", length = 2, nullable = false)
 	private int endTimeWash;
 	
+	@NotNull(message = "Ingrese el pago")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "payment_id")
 	private Payment payment;
